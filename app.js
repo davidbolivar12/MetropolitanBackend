767b12f8ac29d3ecd6af18c03fd95332d7224d89
davidbolivar12 <david.bolivar@utp.edu.co>
2025-01-15 17:58:04 -0500

    se crea el archivo del endpoint 'vehiculos' y se agrega el metodo get

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use("/API/vehiculos", require("./Routes/Vehiculos.js"));
+
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
