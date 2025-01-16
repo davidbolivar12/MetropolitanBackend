d41adfbd875002b4e7365cfec589c65224f35ee2
davidbolivar12 <david.bolivar@utp.edu.co>
2025-01-16 11:40:47 -0500

    se agrega el metodo PUT

--- Routes/Vehiculos.js
+++ Routes/Vehiculos.js
@@ -37,6 +37,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res)=>{
 
     const {modelo, anio, capacidad_pasajeros, capacidad_maletas, tipo_vehiculo} = req.body;
+
     db.query('INSERT INTO vehiculos (modelo, anio, capacidad_pasajeros, capacidad_maletas, tipo_vehiculo) VALUES (?, ?, ?, ?, ?)', [modelo, anio, capacidad_pasajeros, capacidad_maletas, tipo_vehiculo], (err, response)=>{
         if(err){
             return res.status(500).json({ error: "Error en el servidor"});
@@ -46,5 +47,19 @@ router.post('/', (req, res)=>{
         res.json({ message: "vehiculo agregado exitosamente"});
     });
 });
+
+router.put('/', (req, res)=>{
+    const {id} = req.query;
+    const {modelo, anio, capacidad_pasajeros, capacidad_maletas, tipo_vehiculo} = req.body;
+
+    db.query('UPDATE vehiculos set modelo = ?, anio = ?, capacidad_pasajeros = ?, capacidad_maletas = ?, tipo_vehiculo = ? WHERE id_vehiculo = ?', [modelo, anio, capacidad_pasajeros, capacidad_maletas, tipo_vehiculo, id], (err, response)=>{
+        if(err){
+            return res.status(500).json({ error: "Error en el servidor"});
+        }if(response.affectedRows === 0){
+            return res.status(404).json({ error: "Vehiculo no encontrado"})
+        }
+        res.json({message: "Vechiculo actualizado"})
+    })
+})
 
 module.exports = router;
